11c9381bff4e76d2a2595a8ae81b55f77a139291
Panhungting <a0910827893@gmail.com>
2019-07-15 19:14:42 +0900

    0715_2

--- GitTest01/Program.cs
+++ GitTest01/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!0715");
+            Console.WriteLine("Hello World!0715_1");
         }
     }
 }
